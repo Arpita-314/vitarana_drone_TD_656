a9863ef13c3500f78ec3c5192da976b820e62f62
Arpita-314 <59124462+Arpita-314@users.noreply.github.com>
2020-11-25 11:19:03 +0530

    Add files via upload

--- scripts/Task_1_VD_0656_position_controller.py
+++ scripts/Task_1_VD_0656_position_controller.py
@@ -2,130 +2,234 @@
 
 from vitarana_drone.msg import *
 from pid_tune.msg import PidTune
-from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import NavSatFix,LaserScan
 from std_msgs.msg import Float32
 import rospy
 import time
 import tf
+import rosservice
+from std_msgs.msg import String
+from vitarana_drone.srv import *
+import math
+from numpy import inf
 
 class Edrone():
 	def __init__(self):
 		rospy.init_node("position_controller")
 
-		self.set_loc = [19.0000000,72.000045,3.0000] # to ser the goal location from 0.31 altitude to 3m and than displacement in latitude     
-		self.current_loc=[0.0,0.0,0.0] # to store the current location of drone
+		self.set_location = [19.0007046575,71.9998955286,25.000000]      
+		self.final_location= [0.0,0.0,0.0]
+		self.curr_location=[0.0,0.0,0.0]
+		self.pos=[0,0,0]
 
-		self.error = [0,0,0] # to store the eror in latitude,longitude,altitude
-		self.out_throttle=0.0 # to store the required throttle speed
-		self.out_pitch=0.0 # to store the required pitch speed
-		self.out_roll = 0.0
-		self.prev_error=[0.0,0.0,0.0] # to store previous error used in derivative pid error
+		self.lat_error = 0.0
+		self.long_error =0.0 
+		self.alt_error= 0.0
+
+		self.out_throttle=0.0
+		self.out_pitch=0.0
+		self.out_roll=0.0
+		self.prev_error=[0.0,0.0,0.0]
 		
-		#to initialise the prop speed
 		self.rypt_cmd = edrone_cmd() 
 		self.rypt_cmd.rcRoll = 0.0
 		self.rypt_cmd.rcPitch = 0.0
 		self.rypt_cmd.rcYaw = 0.0
 		self.rypt_cmd.rcThrottle = 0.0
 
-		#to store the value to be published of altitude and latitude error
-		self.altitude_error = Float32()
-		self.latitude_error = Float32()
-		self.longitude_error = Float32()
-		self.altitude_error.data = 0
-		self.latitude_error.data = 0
-		self.longitude_error.data = 0
+		# self.alt_kp=41.27
+		# self.alt_ki=0.0
+		# self.alt_kd=3500
+		self.alt_kp=110
+		self.alt_ki=0.0
+		self.alt_kd=4230
 
-		# acts as reference line 
-		self.zero =Float32()
-		self.zero.data=0.0
-		
-		#storing the tuned value of latititude,longitude and altitude respectively
-		self.kp = [32000,19800,110]
-		self.ki = [0,0,0]
-		self.kd = [9500000,5864000,3730]
+		self.lat_kp=32000
+		self.lat_ki=0.0
+		self.lat_kd=9500000
 
-		self.Iterm=[0.0,0.0,0.0]
+		self.long_kp=19800
+		self.long_ki=0.0
+		self.long_kd=5864000
+
+		self.box=[0.0,0.0,0.0]
+		self.flag=1
+
 		self.out_lat=0.0
+
 		self.out_long=0.0
 		self.out_alt=0.0
 
-		self.sample_time = 40
+		self.grip='False'
 
+		self.sample_time = 25
+		self.left_mar=0.0
+		self.back_mar=0.0
+		self.obst_front=0.0
+		self.obst_back=0.0
+		self.obst_right=0.0
+		self.obst_left=0.0
+		self.flag_left=1
+		self.flag_grip=1
+		self.flag_front_yes=1
+		self.flag_left_yes=1
 		self.attitude_input_pub = rospy.Publisher('/drone_command', edrone_cmd,queue_size=1) 
-		self.altitude_out_pub = rospy.Publisher('/altitude_error', Float32,queue_size=1)
-		self.zero_error_pub = rospy.Publisher('/zero_error',Float32,queue_size=1)
-		self.latitude_error_pub = rospy.Publisher('/latitude_error',Float32,queue_size=1) 
-		self.longitude_error_pub = rospy.Publisher('/longitude_error',Float32,queue_size=1) 
+
 		
 		rospy.Subscriber('/edrone/gps', NavSatFix, self.loc_callback)
-		#rospy.Subscriber('/pid_tuning_altitude',PidTune, self.altitude_set_pid)
-		#rospy.Subscriber('/pid_tuning_pitch', PidTune, self.latitude_set_pid)
-		#rospy.Subscriber('/pid_tuning_roll', PidTune, self.longitude_set_pid)
-        	
+		rospy.Subscriber('/pid_tuning_altitude',PidTune, self.altitude_set_pid)
+		rospy.Subscriber('/pid_tuning_pitch', PidTune, self.latitude_set_pid)
+		rospy.Subscriber('/pid_tuning_roll', PidTune, self.longitude_set_pid)
+		rospy.Subscriber('/target',String, self.set_location) 
+		rospy.Subscriber('/edrone/gripper_check', String, self.check_if_available)  
+		rospy.Subscriber('/edrone/range_finder_top',LaserScan,self.set_obs_distances)     	
+
+	def check_if_available(self,ans):
+		self.grip=ans.data
+	
+	def set_obs_distances(self,message):
+		
+		self.obst_front=message.ranges[4]
+		self.obst_back =message.ranges[2]
+		self.obst_right=message.ranges[1]
+		self.obst_left =message.ranges[3]
+
 
 	def loc_callback(self,msg):
-		self.current_loc[0] = msg.latitude
-		self.current_loc[1] = msg.longitude
-		self.current_loc[2] = msg.altitude
-	   
-	def altitude_set_pid(self, altitude):
+		self.curr_location[0] = msg.latitude
+		self.curr_location[1] = msg.longitude
+		self.curr_location[2] = msg.alt
+	
+	def set_location(self,target):
+		loc = target.data
+		loc = loc.replace("'","")
+		loc = loc.replace("data:","")
+		seperate = loc.split(",")
+		self.final_location[0] = float(seperate[0])
+		self.final_location[1] = float(seperate[1])
+		self.final_location[2] = float(seperate[2])
+		self.flag=0
+		print self.final_location
 
-		self.alt_kp = altitude.Kp *1
-		self.alt_ki = altitude.Ki *0.08
-		self.alt_kd = altitude.Kd *2
+	def altitude_set_pid(self, alt):
+
+		self.alt_kp = alt.Kp *1
+		self.alt_ki = alt.Ki *0.08
+		self.alt_kd = alt.Kd *5
 
 	def latitude_set_pid(self, pitch):
 		self.lat_kp = pitch.Kp *100
 		self.lat_ki = pitch.Ki *1
 		self.lat_kd = pitch.Kd *5000
 
-	def latitude_set_pid(self, roll):
-		self.lat_kp = roll.Kp *100
-		self.lat_ki = roll.Ki *1
-		self.lat_kd = roll.Kd *5000	
+
+	def longitude_set_pid(self, roll):
+		self.long_kp = roll.Kp *100
+		self.long_ki = roll.Ki *1
+		self.long_kd = roll.Kd *5000
 
 	def pid_p(self): 
 
-		#calculating current error in altitude
-		self.error[2]=self.set_loc[2] - self.current_loc[2]
+		self.alt_error=self.pos[2] - self.curr_location[2]
 
- 		self.Iterm[2]=(self.Iterm[2]+self.error[2])*self.ki[2]
-		self.out_alt= self.kp[2]*self.error[2] + self.Iterm[2] + (self.error[2] - self.prev_error[2])*self.kd[2] #pid error of altitude
+ 		
+		self.out_alt= self.alt_kp*self.alt_error + self.box[2] + (self.alt_error - self.prev_error[2])*self.alt_kd
 
-		self.altitude_error.data = self.error[2]
+		self.prev_error[2]=self.alt_error
+		self.box[2]=(self.box[2]+self.alt_error)*self.alt_ki
 
-		self.prev_error[2]=self.error[2]
+
+		if(self.curr_location[2]>=25.00 or abs(self.curr_location[0]-self.pos[0])<=0.000001):
+			self.lat_error=self.pos[0] - self.curr_location[0] - self.left_mar
+
+			self.out_lat=self.lat_kp*self.lat_error + self.box[0] + (self.lat_error - self.prev_error[0])*self.lat_kd
+			self.prev_error[0]=self.lat_error
+			self.box[0] =(self.box[0]+self.lat_error)*self.lat_ki
+
+		if(self.curr_location[2]>=25.00 or abs(self.curr_location[1]-self.pos[1])<=0.000001):
+			self.long_error=self.pos[1] - self.curr_location[1] + self.back_mar
+
+			self.out_long=self.long_kp*self.long_error + self.box[1] + (self.long_error - self.prev_error[1])*self.long_kd
+			self.prev_error[1]=self.long_error
+			self.box[1] =(self.box[1]+self.long_error)*self.long_ki
+
+
+
+		if(self.curr_location[0]>=19.0007043000 and self.curr_location[0]<=19.0007047000 and self.flag==1):
+			self.pos[2]=22
+			self.pos[1]=71.9998955286
+			self.pos[0]=19.0007046575
+
+
+
+
+		if(self.grip=='True' and self.flag_grip==1):
+			call_service=rospy.ServiceProxy('/edrone/activate_gripper',Gripper)
+			call_service(True)
+
+			self.pos[0]=self.final_location[0]
+			self.pos[1]=self.final_location[1]
+			self.pos[2]=26
+			self.lat_kp=16000
+			self.long_kp=18000
+			self.flag_grip=0
+
+		
+		if(self.obst_left<=18.0000000 and self.obst_left>=5.0000000 and self.obst_left is not inf and self.obst_left!=0 and self.flag_left_yes==1):
+			print('DONE')
+			print(self.obst_left)
+			self.pos[0]=self.curr_location[0] #- 0.03*(self.obst_left - 8)*0.000009034
+			self.left_mar=(self.obst_left - 8)*0.000009034
+			self.pos[1]=self.curr_location[1] 
+
+			self.back_mar=1.5*0.000009497
+
+
+			if(self.flag_left==1):
+
+				self.out_alt=0.0
+				self.out_lat=0.0
+				self.out_lang=0.0
+
+				self.prev_error=[0.0,0.0,0.0]
+				self.lat_error=0.0
+				self.long_error=0.0
+				self.flag_left=0
 		
 
-		if(self.current_loc[2]==3.00):
-			time.sleep(1)
+		if(self.obst_front<=8.000 and self.obst_front>=2.0000000 and (self.obst_front is not inf) and self.obst_front!=0 and self.flag_front_yes==1):
+			self.back_mar=-1.5*(self.obst_front - 6)*0.000009497
 
-		if(self.current_loc[2]<3.2 and self.current_loc[2]>=3.00): # to calculate the latitude error after 3m altitude is reached
-			self.error[0]=self.set_loc[0] - self.current_loc[0]
+		print(self.obst_front)
 
-			self.Iterm[0] =(self.Iterm[0]+self.error[0])*self.ki[0]
-			self.out_lat=self.kp[0]*self.error[0] + self.Iterm[0] + (self.error[0] - self.prev_error[0])*self.kd[0]# pid error in latitude
-			self.prev_error[0]=self.error[0]
-			self.latitude_error.data = self.error[0]
-
-		#longitude
-		if(self.current_loc[2]<3.2 and self.current_loc[2]>=3.00):
-			self.error[1]=self.set_loc[1] - self.current_loc[1]
-
-			self.Iterm[1] =(self.Iterm[1]+self.error[1])*self.ki[1]
-			self.out_long=self.kp[1]*self.error[1] + self.Iterm[1] + (self.error[1] - self.prev_error[1])*self.kd[1]# pid error in latitude
-			self.prev_error[1]=self.error[1]
-			self.longitude_error.data = self.error[1]
+		if((self.obst_left==inf) and self.pos[0]!=19.0007046575 and self.pos[1]!=71.9998955286 and self.pos[2]!=22):
+			self.pos[0]=self.final_location[0]
+			self.pos[1]=self.final_location[1]
+			self.pos[2]=25
+			# self.back_mar=0.0
+			self.left_mar=0.0
+			self.flag_left_yes=0
+			self.lat_kp=14000
+			self.long_kp=16000
 	
+
+
+		if(self.curr_location[0]>=(self.final_location[0]-0.0001) and self.curr_location[0]<=(self.final_location[0]+0.0001)):
+			self.back_mar=0.0
+			self.lat_kp=26000
+			self.long_kp=28000
 			
-		# to change set point once required altitude and latitude is reached to land the drone on required location
-		if(self.current_loc[1]>=72.000044): 
-			self.set_loc[2]=0.31
+		if(self.curr_location[0]>=(self.final_location[0]-0.00001) and self.curr_location[0]<=(self.final_location[0]+0.00001)):
+			self.pos[2]=6
+				
+
 
 		self.out_throttle= 1500 + self.out_alt
+
+		
 		self.out_pitch=1500 + self.out_lat
-		self.out_roll = 1500 + self.out_long
+
+		self.out_roll=1500 + self.out_long
 
 
 		self.rypt_cmd.rcRoll= self.out_roll
@@ -133,12 +237,9 @@ class Edrone():
 		self.rypt_cmd.rcPitch = self.out_pitch
 		self.rypt_cmd.rcThrottle = self.out_throttle
 
-		self.zero.data = 0
 
-		self.zero_error_pub.publish(self.zero)
-		self.altitude_out_pub.publish(self.altitude_error)
-		self.latitude_error_pub.publish(self.latitude_error)
-		self.longitude_error_pub.publish(self.longitude_error)
+		
+
 		self.attitude_input_pub.publish(self.rypt_cmd)
 
 
